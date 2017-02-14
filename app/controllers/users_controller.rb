c4c430467db7ee28dbc638e7c2dbd34cc102ae49
santiruizt <sruiztoribio@gmail.com>
2017-02-14 23:59:37 +0100

    Add profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+	def profile
+    @user = current_user
+    @docks = @user.docks
+    @boats = @user.boats
+    # @boat = @boats.find_by(id: params[:boat_id])
+    # @rental_requests = @boat.rentals
+    # @rentals = @user.docks.rentals
+  end
+  
 end
